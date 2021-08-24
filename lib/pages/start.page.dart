05d1bc5d30dd887761c1098bcad3f118a40e68bb
Jeremias ramirez <Jereramirez499@gmail.com>
2021-08-24 09:56:41 -0400

    haciendo start page

--- lib/pages/start.page.dart
+++ lib/pages/start.page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_softvpn/widgets/safe.widget.dart';
 
@@ -6,20 +9,32 @@ class StartApp extends StatefulWidget {
 }
 
 class StartAppState extends State<StartApp> {
+  double valueSpinner = .8;
   Widget build(context) {
+    this.accelerateSpinner();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-                centerTitle: true,
-                backgroundColor: Colors.orange[900],
-                title: Text("Verificando conexión")),
-            body: Safe(Center(child: this.spinner()))));
+        home: Scaffold(body: Safe(Center(child: this.spinner()))));
   }
 
-  CircularProgressIndicator spinner() {
-    return CircularProgressIndicator(
-      color: Colors.orange[800],
-    );
+  Widget spinner() {
+    return Roulette(
+        duration: Duration(seconds: 4),
+        child: CircularProgressIndicator(
+          value: this.valueSpinner,
+          color: Colors.orange[800],
+        ));
+  }
+
+  void accelerateSpinner() {
+    Future.delayed(Duration(milliseconds: 3100), () {
+      return true;
+    }).then((value) {
+      if (value) {
+        setState(() {
+          this.valueSpinner = 1;
+        });
+      }
+    });
   }
 }
